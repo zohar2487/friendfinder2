52fe9890dcbf50c9d4c8b0cbfe065c6d566c6e05
zohar <zohar@Andrews-MacBook-Pro.local>
2019-09-29 22:05:51 -0600

    yea

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -31,17 +31,31 @@ module.exports = function(app){
         var sum = b.reduce((a, b) => a = b, 0);
         console.log("Sum of users score " + sum);
         console.log("Best match friend diff " + bestMatch.friendDifference);
-        console.log("--------------------------------------------------------");
+        console.log("++++++++++++++++++++++==================================");
          for (var i = 0; i < friends.length; i++) {
              console.log(friends[i].name);
              totalDiference + 0;
              console.log("Total Diff " + bestMatch.friendDifference);
+            console.log("Best match friend diff " + bestMatch.friendDifference);
 
              var bfriendScore = friends[i].scores.reduce((a,b) => a + b, 0);
              console.log("Total friend score " + bfriendScore);
              totalDiference += Math.abs(sum - bfriendScore);
-             console.log("_______________________________________________> " = totalDiference);
-         }
+             console.log("------------------------------> " + totalDiference);
+
+             if  (totalDiference <= bestMatch.friendDifference) {
+                 bestMatch.name = friends[i].name;
+                 bestMatch.photo = friends[i].photo;
+                 bestMatch.friendDifference = totalDiference;
+               }
+               console.log(totalDiference + " Total Difference");
+            } 
+               console.log(bestMatch);
+               friends.push(userData);
+               console.log("New User added");
+               console.log(userData);
+               res.json(bestMatch);
+         
 
          });
 };
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require("express");
+//var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
@@ -6,13 +6,13 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api=json" }));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("app/public"));
 
-require("./app/")(app);
-require("./app/")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoute")(app);
 
-app.listen(PORT, () => console.log("listening on port%s", PORT))
+app.listen(PORT, () => console.log("listening on port %s", PORT));
